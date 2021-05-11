20f0aacf973a199eff178782674dc1dab6377e13
aladdin731 <aladdin731@hotmail.com>
2021-05-10 23:14:08 -0700

    bubble sort

--- sorting/bubble_sort.js
+++ sorting/bubble_sort.js
@@ -1,8 +1,57 @@
+// time: O(n2)
+// space: O(n)
+
+function swap(arr, index1, index2) {
+    let temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+}
+
+function bubbleSort(arr) {
+    // track whether or not we made a swap on the previous pass
+    let swapped = true;
+
+
+    while(swapped) { // O(n)
+        swapped = false;
+        for(let i = 0; i < arr.length - 1; i++) { // O(n)
+            if(arr[i] > arr[i + 1]) {
+                swap(arr, i, i + 1);
+                swapped = true;
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(bubbleSort([2,3,1,-1]))
+
+// Time Complexity: O(n2) completely unsorted
+// n is the length of the inut array
+// The inner for loop along contributes O(n) in isolation
+// The outer while loop contributes O(n) in isolation 
+// because a single iteration of the while loop will bring one element to it's final resting position. 
+// In other words, we keep running the while loop until the array is fully sorted. 
+// To fully sort the array we will need to bring all n elements into their final resting positions.
+
+// best case: no need to do outer loop
+
+
+// Space: O(1)
+// The amount of memory does not increase relative to the size of the input array. 
+// The space efficiency mostly comes from the fact that we mutate the input array, in-place.
+
 // useful educationally
 // but is almost never a good choice in industry
 // not efficient
 // not commonly used
 
 // bubbling up 往上冒泡
+// 每次都把未排序中的最大的冒泡到最右到已排序中
+// 每两个比较 大的往右
 
-// 
+// As we iterate through the array, we compare each element to its right neighbor. 
+// If the current element is larger than its neighbor, we swap them.
+
+// During Bubble Sort, we can tell if the array is in sorted order 
+// by checking if we made a swap during the previous pass performed. 
--- sorting/merge_sort.js
+++ sorting/merge_sort.js
@@ -1,3 +1,6 @@
+// time: O(nlog(n)) 
+// space: O(n)
+
 function mergeSort(arr) {
     if(arr.length <= 1) return arr;
 
@@ -27,10 +30,28 @@ function merge(left, right) {
 // Time complexity : O(nlog(n)) 
 // divide O(logn) merge(n)
 
+// We must calculate how many recursive calls we make. 
+// The number of recursive calls is the number of times we must split the array to reach the base case. 
+// Since we split in half each time, the number of recursive calls is O(log(n)).
+
+// For example, say we had an array of length 32,
+// then the length would change as 32 -> 16 -> 8 -> 4 -> 2 -> 1, 
+// we have to split 5 times before reaching the base case, log(32) = 5,
+// In our algorithm, log(n) describes how many times we must halve n until the quantity reaches 1.
+
+// Besides the recursive calls, the while loop within the merge function, which contributes O(n) in isolation.
+// We call merge in every recursive mergeSort call, so the total complexity is O(n * log(n))
+
+
 // Space complesity: O(n) 
 // need a new subarray for each element in the original input
 
-//Unless we, the engineers, have access in advance to some unique, 
+// The larger the size of our input array, the greater the number of subarrays we must create in memory. 
+// These are not free! They each take up finite space, 
+// and we will need a new subarray for each element in the original input. 
+// Therefore, Merge Sort has a linear space complexity, O(n).
+
+// Unless we, the engineers, have access in advance to some unique, 
 // exploitable insight about our dataset, 
 // it turns out that O(n log n) time is the best we can do when sorting unknown datasets.
 
